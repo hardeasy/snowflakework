48607fcea0d4eceb6bc836776f4cf6b89e7e909b
wenxh <56919916@qq.com>
2017-11-07 23:15:59 +0800

    base

---
+++ snowflakework.go
@@ -0,0 +1,47 @@
+package snowflakework
+
+import (
+	"time"
+)
+
+//0 41 10 12
+
+var shiftTime uint64 = 1483200000000 //2017-01-01
+var count uint64 = 0                 //
+
+func getNowTimeStamp() uint64 {
+	timestamp := uint64(time.Now().UnixNano() / 1000000)
+	return timestamp - shiftTime
+}
+
+func getWorkId() uint64 {
+	var mask uint64 = 0x3ff
+	return mask & 1
+}
+
+func getCount(timestamp uint64) uint64 {
+	var mask uint64 = 0xfff
+	count++
+	count = count & mask
+	if count == 0 {
+		//wait next
+		for {
+			nowTimeStamp := getNowTimeStamp()
+			if nowTimeStamp > timestamp {
+				break
+			}
+		}
+		return 0
+	}
+	return count
+}
+
+func MakeId() uint64 {
+	time := getNowTimeStamp()
+	workid := getWorkId()
+	count := getCount(time)
+
+	id := (time << 22) | (workid << 12) | count
+
+	return id
+}
